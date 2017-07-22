6f76dab42d9aa2b2867bf260d41732d7a49b468d
Nisodemus <phoxlabmail@gmail.com>
2017-07-22 18:30:15 +0100

    udp client and v2 of TCPserver

--- ch2/TCPClient.py
+++ ch2/TCPClient.py
@@ -4,8 +4,9 @@
 
 import socket
 
-target_host = "www.google.com"
-target_port = 80
+#host currently set to the TCPServer included in folder
+target_host = "0.0.0.0"
+target_port = 9999
 
 #create a socket object
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -15,11 +16,21 @@ client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # connect the client
 client.connect((target_host,target_port))
 
+while True:
+    #send data to client
+    client.send(b"AABBBBCCCDDDTESTDATA101")
+
+    #recieve data from target_host
+    response = client.recv(4096)
+
+
 #send some data - create the var and then use str.encode() due to it expecting
 #bytes but data is a unicode string
-data = "GET / HTTP/1.1\r\nHost: google.com\r\n\r\n"
-client.send(data.encode('utf-8'))
+#data = "GET / HTTP/1.1\r\nHost: google.com\r\n\r\n"
+#client.send(data.encode('utf-8'))
 
 #recieve some data and print response
-response = client.recv(4096).decode('utf-8')
+#response = client.recv(4096).decode('utf-8')
+
 print(response)
+time.sleep(2)
---
+++ ch2/TCPServerV2.py
@@ -0,0 +1,45 @@
+import socket
+import threading
+
+
+bind_ip = "0.0.0.0"
+bind_port = 9999
+
+# Create instance of a socket to act as server
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Pass IP and Port for server to listen to
+server.bind((bind_ip, bind_port))
+
+# Set max connection backlog to 5
+server.listen(5)
+
+print("[*] Listening on %s:%d" % (bind_ip, bind_port))
+
+
+def handle_client(client_socket):
+    while True:
+        # Capture data recieved from client
+        request = client_socket.recv(1024)
+
+        print("[*] Received: %s" % request)
+
+        try:
+            # Send packet back to client
+            client_socket.send(b"Received")
+        except:
+            # Close connection if client couldn't receive
+            client_socket.close()
+            break
+
+
+
+while True:
+
+    client, addr = server.accept()
+
+    print("[*] Accepted connection from: %s:%d" % (addr[0], addr[1]))
+
+    # Spin client thread to handle incoming data
+    client_handler = threading.Thread(target=handle_client, args=(client,))
+    client_handler.start()
