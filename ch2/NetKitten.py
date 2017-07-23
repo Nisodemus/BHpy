d1ff3559d674d9ddb4bf24d2f699cfd49e1ac26c
Nisodemus <phoxlabmail@gmail.com>
2017-07-23 18:11:57 +0100

    client handler threading

--- ch2/NetKitten.py
+++ ch2/NetKitten.py
@@ -43,7 +43,7 @@ def main ():
 
     #Decide if we are listening or just sending data from stdin
     if not listen and target is not None and port > 0:
-        print("Read buffer from stdin")
+        print("Kitten: Read buffer from stdin")
         buff = sys.stdin.read()
         #note - this will blockup so CTRL-D is not sending to stdin
         print("Sending [{0} to client".format(buff))
@@ -54,4 +54,74 @@ def main ():
     if listen:
         server_loop()
 
-        
+def client_sender(buff):
+
+    print("Kitten: Sending data to client on port " + str(port))
+
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    try:
+        # connection to target
+        client.connect((target, port))
+
+        if len(buff):
+            client.send(buff.encode())
+        while True:
+            #Wait for a data response
+            recv_len = 1
+            response = ""
+
+            while recv_len:
+                print("Kitten: waiting for response form client")
+                data = client.recv(4096)
+                recv_len = len(data)
+                response += data.decode(errors="ignore")
+
+                if recv_len < 4096:
+                    break
+
+            print(response, end="")
+
+            #wait for more inputs
+            buff = input("")
+            buff += "\n"
+
+            #ship it away
+            client.send(buff.encode())
+
+        except:
+            print("[] Exception n shit. Run Away!")
+        finally:
+            client.close()
+
+def server_loop():
+
+    global target
+    print("kitten: Entering server loopage")
+
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind((target, port))
+    #set max listen to 5
+    server.listen(5)
+
+    while True:
+        client_socket, addr = server.accept()
+
+        #spin up thread to handle new client
+        client_thread = threading_Thread(target=client_handler, args=(client_socket,))
+
+def run_command(command):
+    #newline prunning
+    command = command.rstrip()
+    print("Kitten: Firing command: " + command)
+
+    try:
+        #launch new process
+        output = subprocess.check_output(command, stderr=subprocess.STDOUT, shell=True)
+    except:
+        output = "Failed to fire command. \r\n"
+
+    #shoot data back to client
+    return output
+
+#client handler + shell loop + shit tons of fixs needed. 
