ad397785611373dbde7ee2dc858effaed20a9966
Nisodemus <phoxlabmail@gmail.com>
2017-07-23 19:39:40 +0100

    V1 - (also fixed args parser)

--- ch2/NetKitten.py
+++ ch2/NetKitten.py
@@ -14,7 +14,8 @@ upload = None
 command = False
 listen = False
 
-def main ():
+
+def main():
 
     global target
     global port
@@ -27,11 +28,12 @@ def main ():
     #build argument parsing and descriptions of various actions
     parser = argparse.ArgumentParser(description="Baby netcat clone built in py3")
     parser.add_argument("port", type=int, help="The target port")
-    parser.add_argument("-t", "--target_host", type.str, help="The target host", default="0.0.0.0")
+    parser.add_argument("-t", "--target_host", type=str, help="The target host", default="0.0.0.0")
     parser.add_argument("-l", "--listen", help="listen on [host]:[port] for connections", action="store_true", default=False)
     parser.add_argument("-e", "--execute", help="--execute=file_to_run fire the given file when receiving a connection")
     parser.add_argument("-c", "--command", help="Spawn a command shell", action="store_true", default=False)
     parser.add_argument("-u", "--upload", help="--upload=destination when upload file and write to [destination] when connection recieved")
+    args = parser.parse_args()
 
     #parse arguments
     target = args.target_host
@@ -43,6 +45,7 @@ def main ():
 
     #Decide if we are listening or just sending data from stdin
     if not listen and target is not None and port > 0:
+
         print("Kitten: Read buffer from stdin")
         buff = sys.stdin.read()
         #note - this will blockup so CTRL-D is not sending to stdin
@@ -53,6 +56,7 @@ def main ():
     #Let's listen and upload, execute and drop shells back (depending on options)
     if listen:
         server_loop()
+
 
 def client_sender(buff):
 
@@ -89,10 +93,11 @@ def client_sender(buff):
             #ship it away
             client.send(buff.encode())
 
-        except:
-            print("[] Exception n shit. Run Away!")
-        finally:
-            client.close()
+    except:
+        print("[] Exception n shit. Run Away!")
+    finally:
+        client.close()
+
 
 def server_loop():
 
@@ -110,6 +115,7 @@ def server_loop():
         #spin up thread to handle new client
         client_thread = threading_Thread(target=client_handler, args=(client_socket,))
 
+
 def run_command(command):
     #newline prunning
     command = command.rstrip()
@@ -124,8 +130,8 @@ def run_command(command):
     #shoot data back to client
     return output
 
-#client handler stuff
 
+#client handler stuff
 def client_handler(client_socket):
     global execute
     global command
@@ -141,6 +147,7 @@ def client_handler(client_socket):
 
         #keep reading data untill none remains
         while True:
+
             data = client_socket.recv(1024)
             if not data:
                 break
@@ -148,6 +155,7 @@ def client_handler(client_socket):
 
         #wreite bytes to file
         try:
+
             cock = open(uploadDestination, "wb")
             cock.write(file_buff)
             cock.close()
@@ -158,6 +166,7 @@ def client_handler(client_socket):
             client_socket.send("Aww Shit - Failed to save at {0}. Does this dir even exist?\r\n".format(uploadDestination).encode())
 
     if execute is not None:
+
         print("Kitten: Racking command")
         #fire command
         output = run_command(execute)
@@ -165,12 +174,12 @@ def client_handler(client_socket):
 
     #if shell was requested fall into loop
     if command:
+
         print("Kitten: pls can i have shell")
         #prompt
         client_socket.send("<BHP:#>".encode())
 
         while True:
-
             #recieve until linefeed
             cmd_buff = ""
             while "\n" not in cmd_buff:
@@ -184,5 +193,6 @@ def client_handler(client_socket):
 
             #send back response
             client_socket.send(response + "<BHP:#>".encode())
+
 
 main()
